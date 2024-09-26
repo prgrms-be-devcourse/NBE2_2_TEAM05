505ca97919054a384f34b20374d385d289fde13a
Hyeongyu <delldro94@gmail.com>
2024-09-26 10:52:52 +0900

    test2

--- src/main/java/edu/example/dev_2_cc/dto/testDTO.java
+++
@@ -1,4 +0,0 @@
-package edu.example.dev_2_cc.dto;
-
-public class testDTO {
-}
--- src/main/java/edu/example/dev_2_cc/service/CartService.java
+++ src/main/java/edu/example/dev_2_cc/service/CartService.java
@@ -1,4 +1,27 @@
 package edu.example.dev_2_cc.service;
 
+import edu.example.dev_2_cc.repository.CartItemRepository;
+import edu.example.dev_2_cc.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    //장바구니 등록
+
+    //장바구니 조회
+
+    //장바구니 전체조회
+
+    //장바구니 수정
+
+    //장바구니 삭제
+
+
+
 }
+
