d07634e66094e4b4f6db66e83cd447fff484aab1
chadoli27 <jasminekim1009@gmail.com>
2024-10-02 09:45:13 +0900

    댓글 게시물별 리스트

--- src/main/java/edu/example/dev_2_cc/api_controller/ReplyController.java
+++ src/main/java/edu/example/dev_2_cc/api_controller/ReplyController.java
@@ -1,5 +1,6 @@
 package edu.example.dev_2_cc.api_controller;
 
+import edu.example.dev_2_cc.dto.reply.ReplyListDTO;
 import edu.example.dev_2_cc.dto.reply.ReplyRequestDTO;
 import edu.example.dev_2_cc.dto.reply.ReplyResponseDTO;
 import edu.example.dev_2_cc.service.ReplyService;
@@ -8,6 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -33,6 +35,12 @@ public class ReplyController {
     public ResponseEntity<Map<String, String>> deleteReply(@PathVariable("replyId") Long replyId) {
         replyService.delete(replyId);
         return ResponseEntity.ok(Map.of("message", "Reply deleted"));
+    }
+
+    @GetMapping("/listByBoard/{boardId}")
+    public ResponseEntity<List<ReplyListDTO>> listByBoardId(@PathVariable("boardId") Long boardId) {
+        List<ReplyListDTO> replies = replyService.list(boardId);
+        return ResponseEntity.ok(replies);
     }
 
     
--- src/main/java/edu/example/dev_2_cc/repository/ReplyRepository.java
+++ src/main/java/edu/example/dev_2_cc/repository/ReplyRepository.java
@@ -1,8 +1,13 @@
 package edu.example.dev_2_cc.repository;
 
+import edu.example.dev_2_cc.dto.reply.ReplyListDTO;
 import edu.example.dev_2_cc.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+    @Query("SELECT r FROM Reply r WHERE r.board.boardId = :boardId")
+    List<Reply> findAllByBoard(Long boardId);
 }
