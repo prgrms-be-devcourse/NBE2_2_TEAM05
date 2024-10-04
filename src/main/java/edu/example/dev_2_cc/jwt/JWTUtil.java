77555985a071cbb38f78babc5302c8b6edb7535a
lǐ_róng_mín <96493001+Yi-YongMin@users.noreply.github.com>
2024-10-04 17:36:19 +0900

    ãboard Exception

--- src/main/java/edu/example/dev_2_cc/jwt/JWTUtil.java
+++ src/main/java/edu/example/dev_2_cc/jwt/JWTUtil.java
@@ -44,7 +44,7 @@ public class JWTUtil {
                 .claim("memberId", memberId)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs*1000))
                 .signWith(secretKey)
                 .compact();
     }
